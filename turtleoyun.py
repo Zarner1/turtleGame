4834409989f2c87d74d880714d9390744696d0ff
Muhammed Sait Yıldırım <yildirim340458@gmail.com>
2024-05-05 03:46:07 +0300

    Initial commit

---
+++ turtleoyun.py
@@ -0,0 +1,46 @@
+import turtle
+import random
+
+d_b = turtle.Screen()
+d_b.bgcolor("grey")
+turt1 = turtle.Turtle()
+turt2 = turtle.Turtle()
+turt1.hideturtle()
+turt1.penup()
+turt2.penup()
+
+turt1.setposition(0, 285)
+i = 0
+turt1.write("Skor: 0", move=False, align="center", font=("Verdana", 15, "normal"))
+
+
+def turtleP():
+    d_b.tracer(0)
+    turt2.speed(1)
+    x = random.randint(a=-250, b=250)
+    y = random.randint(a=-280, b=280)
+    turt2.setposition(x, y)
+    turt2.showturtle()
+    turt2.shape("turtle")
+    turt2.turtlesize(2)
+    d_b.tracer(1)
+    d_b.delay(1000)
+    turt2.clear()
+
+
+
+def skor(x,y):
+    global turt2
+    global i
+    turt2_pos=turt2.position()
+    if abs(x-turt2_pos[0] )<=3 and abs(y-turt2_pos[1] )<=3:
+      turt1.clear()
+      i += 1
+      turt1.write("Skor: " + str(i), move=False, align="center", font=("Verdana", 15, "normal"))
+
+
+for x in range(9999):
+
+    turtleP()
+    turt2.onclick(skor)
+d_b.mainloop()
