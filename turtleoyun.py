512f186f5f0198f6c6fcd293905d6b603d0a0b36
Muhammed Sait Yıldırım <yildirim340458@gmail.com>
2024-05-05 13:27:41 +0300

    Finish

--- turtleoyun.py
+++ turtleoyun.py
@@ -16,7 +16,7 @@ turt1.write("Skor: 0", move=False, align="center", font=("Verdana", 15, "normal"
 
 def turtleP():
     d_b.tracer(0)
-    turt2.speed(1)
+
     x = random.randint(a=-250, b=250)
     y = random.randint(a=-280, b=280)
     turt2.setposition(x, y)
@@ -24,23 +24,22 @@ def turtleP():
     turt2.shape("turtle")
     turt2.turtlesize(2)
     d_b.tracer(1)
-    d_b.delay(1000)
+    d_b.delay(500)
     turt2.clear()
 
 
-
-def skor(x,y):
+def skor(x, y):
     global turt2
     global i
-    turt2_pos=turt2.position()
-    if abs(x-turt2_pos[0] )<=3 and abs(y-turt2_pos[1] )<=3:
-      turt1.clear()
-      i += 1
-      turt1.write("Skor: " + str(i), move=False, align="center", font=("Verdana", 15, "normal"))
+    turt2_pos = turt2.position()
+    if abs(x - turt2_pos[0]) <= 50 and abs(y - turt2_pos[1]) <= 50:
+        turt1.clear()
+        i += 1
+        turt1.write("Skor: " + str(i), move=False, align="center", font=("Verdana", 15, "normal"))
 
 
-for x in range(9999):
-
+while True:
     turtleP()
     turt2.onclick(skor)
+
 d_b.mainloop()
